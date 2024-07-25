cfe5636aaf11691d6ef7a1da5383ae607330616b
JakubKlipkon <jakub.rudnik@siecbudowlanaexport.pl>
2024-07-25 15:51:15 +0200

    types, layout and hero section

---
+++ src/components/ui/button.tsx
@@ -0,0 +1,55 @@
+import { Slot } from "@radix-ui/react-slot";
+import { cva, type VariantProps } from "class-variance-authority";
+import * as React from "react";
+
+import { cn } from "src/lib/utils";
+
+const buttonVariants = cva(
+  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  {
+    variants: {
+      variant: {
+        default:
+          "from-secondary to-primary bg-gradient-45 text-white hover:bg-gradient-up hover:shadow-blue-strong transition-shadow",
+        destructive:
+          "bg-destructive text-destructive-foreground hover:bg-destructive/90",
+        outline:
+          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+        secondary: "bg-secondary/15 text-primary hover:bg-secondary/25",
+        ghost: "hover:bg-accent hover:text-accent-foreground",
+        link: "text-primary underline-offset-4 hover:underline",
+      },
+      size: {
+        default: "px-9 py-3",
+        sm: "rounded-md px-4 px-2",
+        icon: "p-2",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+    },
+  }
+);
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  asChild?: boolean;
+}
+
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, variant, size, asChild = false, ...props }, ref) => {
+    const Comp = asChild ? Slot : "button";
+    return (
+      <Comp
+        className={cn(buttonVariants({ variant, size, className }))}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
+Button.displayName = "Button";
+
+export { Button, buttonVariants };
--- src/types/page.ts
+++ src/types/page.ts
@@ -1,14 +1,64 @@
-export interface Page {
+export interface IPage {
   id: number;
-  attributes: PageAttributes;
+  attributes: IPageAttributes;
 }
 
-export interface PageAttributes {
+export interface IPageAttributes {
   createdAt: Date;
   updatedAt: Date;
   publishedAt: Date;
   slug: string;
-  components: Component[];
+  components: IComponent[];
+  seo: ISeo;
 }
 
-export interface Component {}
+export interface IComponent {
+  id: number;
+  __component: string;
+  image?: IImage;
+  buttons: IButton[];
+}
+
+export interface ISeo {
+  id: number;
+  metaTitle: string;
+  metaDescription: string;
+  keywords?: string;
+  metaRobots?: string;
+  structuredData?: string;
+  metaViewport?: string;
+  canonicalURL?: string;
+}
+
+export interface IButton {
+  id: number;
+  content: string;
+  size: "default" | "sm" | "icon";
+  variant: "default" | "secondary" | "ghost";
+  href: string;
+  icon: string;
+}
+
+export interface IImage {
+  data: {
+    id: number;
+    attributes: {
+      name: string;
+      alternativeText: string;
+      caption: null;
+      width: number;
+      height: number;
+      formats: null;
+      hash: string;
+      ext: string;
+      mime: string;
+      size: number;
+      url: string;
+      previewUrl: null;
+      provider: string;
+      provider_metadata: null;
+      createdAt: string;
+      updatedAt: string;
+    };
+  };
+}
